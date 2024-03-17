6eaa84fff8feae4a275579f1766b750d3739b834
Traxar <trxr2a@gmail.com>
2024-03-17 19:34:29 +0100

    minimize allocations updated to fit vector & matrix

--- src/struct/permutation.zig
+++ src/struct/permutation.zig
@@ -9,50 +9,50 @@ pub fn PermutationType(comptime Index: type) type {
     return struct {
         const Permutation = @This();
         val: [*]Index,
-        inv: [*]Index,
         len: Index,
 
         /// allocate permutation of size n with undefined values
-        fn init(n: Index, allocator: Allocator) !Permutation {
+        pub fn init(n: Index, allocator: Allocator) !Permutation {
             return Permutation{
-                .val = (try allocator.alloc(Index, n)).ptr,
-                .inv = (try allocator.alloc(Index, n)).ptr,
+                .val = (try allocator.alloc(Index, 2 * n)).ptr,
                 .len = n,
             };
         }
 
+        ///allocate permutation with undefined values and same dimensions as a
+        pub fn like(a: Permutation, allocator: Allocator) !Permutation {
+            return init(a.len, allocator);
+        }
+
         /// deinitialize permutation
         pub fn deinit(a: Permutation, allocator: Allocator) void {
-            allocator.free(a.val[0..a.len]);
-            allocator.free(a.inv[0..a.len]);
+            allocator.free(a.val[0 .. 2 * a.len]);
         }
 
-        /// allocate identity permutation of size n
-        pub fn eye(n: Index, allocator: Allocator) !Permutation {
-            var res = try init(n, allocator);
-            for (0..res.len) |i| {
+        /// set permutation to identity
+        pub fn eye(res: Permutation) void {
+            var i: Index = 0;
+            const n = res.len;
+            while (i < n) : (i += 1) {
                 res.val[i] = i;
             }
-            @memcpy(res.inv[0..n], res.val[0..n]);
-            return res;
+            @memcpy(res.val[n .. 2 * n], res.val[0..n]);
         }
 
-        /// allocate copy of permutation a
-        pub fn copy(a: Permutation, allocator: Allocator) !Permutation {
-            return Permutation{
-                .val = (try allocator.dupe(Index, a.val[0..a.len])).ptr,
-                .inv = (try allocator.dupe(Index, a.inv[0..a.len])).ptr,
-                .len = a.len,
-            };
+        /// res <- a
+        pub fn copy(a: Permutation, res: Permutation) void {
+            assert(a.len == res.len);
+            const n = res.len;
+            @memcpy(res.val[0 .. 2 * n], a.val[0 .. 2 * n]);
         }
 
-        /// allocte inverse permuatation
-        pub fn inv(a: Permutation, allocator: Allocator) !Permutation {
-            return Permutation{
-                .val = (try allocator.dupe(Index, a.inv[0..a.len])).ptr,
-                .inv = (try allocator.dupe(Index, a.val[0..a.len])).ptr,
-                .len = a.len,
-            };
+        /// res <- a^-1
+        pub fn inv(a: Permutation, res: Permutation) void {
+            assert(a.len == res.len);
+            assert(a.val != res.val); //inplace
+            const n = res.len;
+            @memcpy(res.val[0..n], a.val[n .. 2 * n]);
+            @memcpy(res.val[n .. 2 * n], a.val[0..n]);
         }
 
         /// return target at index
@@ -62,7 +62,7 @@ pub fn PermutationType(comptime Index: type) type {
 
         ///return source of index
         pub inline fn atInv(a: Permutation, i: Index) Index {
-            return a.inv[i];
+            return a.val[a.len + i];
         }
 
         /// swaps the two elements at i and j in the permutation.
@@ -72,17 +72,17 @@ pub fn PermutationType(comptime Index: type) type {
             const value_j = a.val[j];
             a.val[i] = value_j;
             a.val[j] = value_i;
-            a.inv[value_i] = j;
-            a.inv[value_j] = i;
+            a.val[a.len + value_i] = j;
+            a.val[a.len + value_j] = i;
         }
 
         /// swaps the two elements at i and j in the initial configuration.
         /// The operation is performed on a.
         pub fn swapInv(a: Permutation, i: Index, j: Index) Permutation {
-            const value_i = a.inv[i];
-            const value_j = a.inv[j];
-            a.inv[i] = value_j;
-            a.inv[j] = value_i;
+            const value_i = a.val[a.len + i];
+            const value_j = a.val[a.len + j];
+            a.val[a.len + i] = value_j;
+            a.val[a.len + j] = value_i;
             a.val[value_i] = j;
             a.val[value_j] = i;
             return a;
@@ -93,7 +93,7 @@ pub fn PermutationType(comptime Index: type) type {
             assert(a.len == b.len);
             for (0..a.len) |i| {
                 a.val[i] = b.at(a.val[i]);
-                a.inv[a.val[i]] = i;
+                a.val[a.len + a.val[i]] = i;
             }
         }
 
@@ -101,8 +101,9 @@ pub fn PermutationType(comptime Index: type) type {
         pub fn apply(a: Permutation, b: Permutation) void {
             assert(a.len == b.len);
             for (0..a.len) |i| {
-                a.inv[i] = b.atInv(a.inv[i]);
-                a.val[a.inv[i]] = i;
+                const j = a.len + i;
+                a.val[j] = b.atInv(a.val[j]);
+                a.val[a.val[j]] = i;
             }
         }
     };
@@ -113,8 +114,9 @@ test "permutation creation" {
     const P = PermutationType(usize);
 
     const n = 100;
-    const p = try P.eye(n, ally);
+    const p = try P.init(n, ally);
     defer p.deinit(ally);
+    p.eye();
 
     for (0..n) |i| {
         try testing.expectEqual(i, p.at(i));
@@ -126,8 +128,9 @@ test "permutation operations" {
     const P = PermutationType(usize);
 
     const n = 4;
-    var a = try P.eye(n, ally);
+    var a = try P.init(n, ally);
     defer a.deinit(ally);
+    a.eye();
     a.swap(1, 2);
     a.swap(0, 1);
     try testing.expectEqual(@as(usize, 2), a.at(0));
@@ -140,8 +143,9 @@ test "permutation operations" {
     try testing.expectEqual(@as(usize, 0), a.atInv(2));
     try testing.expectEqual(@as(usize, 3), a.atInv(3));
 
-    var b = try P.eye(n, ally);
+    var b = try P.like(a, ally);
     defer b.deinit(ally);
+    b.eye();
     b.swap(0, 3);
     b.apply(a);
     try testing.expectEqual(@as(usize, 2), b.at(0));
@@ -154,8 +158,9 @@ test "permutation operations" {
     try testing.expectEqual(@as(usize, 0), b.atInv(2));
     try testing.expectEqual(@as(usize, 1), b.atInv(3));
 
-    var c = try P.inv(b, ally);
+    var c = try P.like(b, ally);
     defer c.deinit(ally);
+    b.inv(c);
     c.applyTo(b);
     try testing.expectEqual(@as(usize, 0), c.at(0));
     try testing.expectEqual(@as(usize, 1), c.at(1));
